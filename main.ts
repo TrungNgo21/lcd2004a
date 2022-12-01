fd301bfba176b6a526ccf6eed17f8a5c2bef0abc
TrungNgo21 <90534385+TrungNgo21@users.noreply.github.com>
2022-12-01 10:48:43 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 I2C_LCD1602.LcdInit(39)
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P3, 1)
 })
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P3, 1)
+    pins.digitalWritePin(DigitalPin.P1, 1)
 })
 basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P0, 1)
@@ -22,13 +22,13 @@ basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 0) {
         I2C_LCD1602.ShowString("Available slots:", 0, 0)
         if (pins.digitalReadPin(DigitalPin.P2) == 0) {
-            if (pins.digitalReadPin(DigitalPin.P3) == 1) {
+            if (pins.digitalReadPin(DigitalPin.P3) == 0) {
                 I2C_LCD1602.ShowString("Slot 2: 0 Slot 3: 0 Slot 4: 0", 10, 2)
             } else {
                 I2C_LCD1602.ShowString("Slot 2: 0 Slot 3: 0 Slot 4: 1", 10, 2)
             }
         } else {
-            if (pins.digitalReadPin(DigitalPin.P3) == 1) {
+            if (pins.digitalReadPin(DigitalPin.P3) == 0) {
                 I2C_LCD1602.ShowString("Slot 2: 0 Slot 3: 1 Slot 4: 0", 10, 2)
             } else {
                 I2C_LCD1602.ShowString("Slot 2: 0 Slot 3: 1 Slot 4: 1", 10, 2)
@@ -37,13 +37,13 @@ basic.forever(function () {
     } else {
         I2C_LCD1602.ShowString("Available slots:", 0, 0)
         if (pins.digitalReadPin(DigitalPin.P2) == 0) {
-            if (pins.digitalReadPin(DigitalPin.P3) == 1) {
+            if (pins.digitalReadPin(DigitalPin.P3) == 0) {
                 I2C_LCD1602.ShowString("Slot 2: 1 Slot 3: 0 Slot 4: 0", 10, 2)
             } else {
                 I2C_LCD1602.ShowString("Slot 2: 1 Slot 3: 0 Slot 4: 1", 10, 2)
             }
         } else {
-            if (pins.digitalReadPin(DigitalPin.P3) == 1) {
+            if (pins.digitalReadPin(DigitalPin.P3) == 0) {
                 I2C_LCD1602.ShowString("Slot 2: 1 Slot 3: 1 Slot 4: 0", 10, 2)
             } else {
                 I2C_LCD1602.ShowString("Slot 2: 1 Slot 3: 1 Slot 4: 1", 10, 2)
